b6783c5fd50c39c09d09baf3ad9ba887133f64e7
veku <veku@cyber-host.net>
2019-05-06 15:05:52 -0400

    Is dit wel nodig?

--- platforms/java/src/main/java/io/fire/core/common/interfaces/ClientMeta.java
+++
@@ -1,8 +0,0 @@
-package io.fire.core.common.interfaces;
-
-public interface ClientMeta {
-
-    //ClientMeta interface
-    //used by Client and Server
-
-}
--- platforms/java/src/main/java/io/fire/core/common/interfaces/PoolHolder.java
+++
@@ -1,9 +0,0 @@
-package io.fire.core.common.interfaces;
-
-import io.fire.core.common.objects.ThreadPool;
-
-public interface PoolHolder {
-
-    ThreadPool getPool();
-
-}
